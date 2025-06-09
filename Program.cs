b0e93ba351c86c0486744c25b657d31125f73c7a
topherjn <nicholsonc@gmail.com>
2025-06-09 17:01:12 -0400

    Specials data properly populated from database

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@ app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
 // Initialize the database
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 using (var scope = scopeFactory.CreateScope())
